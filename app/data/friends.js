a12779cb17cbccabd1eb91d807fd907df17e5970
vilagen <vilagen@yahoo.com>
2019-07-19 22:48:00 -0400

    figured out logic, now need to route it.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,41 +1,52 @@
-var friendsArray = [
+friendsArray = [
     {
         "name": "David Hart",
+        "link": "https://vilagen.github.io/Responsive-Portfolio/",
         "scores": [
         "1",
         "2",
         "5",
         "4",
-        "3"
+        "3",
+        "4",
+        "2",
+        "5",
+        "1",
+        "2"
         ]
     },
     {
-        "name": "Steven Bob",
+        "name": "Megan Markle",
+        "link": "https://www.hellomagazine.com/imagenes/royalty/2017120144448/megan-markle-first-engagement-soon/0-225-150/meghan-markle-t.jpg",
         "scores": [
         "4",
         "5",
         "1",
         "4",
+        "1",
+        "1",
+        "4",
+        "4",
+        "3",
         "1"
         ]
     },
     {
-        "name": "John Picord",
+        "name": "Jean-Luc Picard",
+        "link": "https://cdn1-www.comingsoon.net/assets/uploads/2018/12/Picard.jpg",
         "scores": [
         "1",
         "1",
         "5",
         "5",
-        "5"
+        "5",
+        "3",
+        "2",
+        "4",
+        "4",
+        "1"
         ]
     }
-
 ]
-
-function findFriend(arr) {
-    for(var i = 0; i < arr.length; i++){
-        console.log(arr.scores[i])
-    }
-}
 
 module.exports = friendsArray
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,12 +6,49 @@ module.exports = function(app) {
     })
 
     app.post("/api/friends", function(req, res) {
-
         // input logic 
         // let friend = req.body
 
         friends.push(req.body)
         res.json(true)
+
+        tallyScores(friendsArray, newFriend)
+       
+
+        function tallyScores(arr, user) {
+            var friendScore = []
+            var total = []
+            for(var i = 0; i < arr.length; i++){
+                scores = arr[i].scores
+                userScores = user.scores
+                for( var j = 0; j < scores.length; j++) {
+                    total.push(parseInt(Math.abs(userScores[j] - scores[j])))
+                }
+                let sum = total.reduce((acc, val) => {
+                    return acc + val
+                });
+                friendScore.push({name: arr[i].name, link: arr[i].link, score: sum})
+                var total = []
+            }
+            friendScore.pop()
+            friendFinder(friendScore)
+            
+        }
+        
+        function friendFinder(arr){
+            var bestMatch = arr[0]
+            for(var i = 0; i < arr.length; i++){
+                if(arr[i].score < bestMatch.score) {
+                    bestMatch = arr[i]
+                }
+            }
+            $("#matchedFriend").text("Name: " + bestMatch.name)
+            $("#matchedLink").attr("href", bestMatch.link)
+            $("#matchedLink").on("click", function(event){
+                event.preventDefault();
+                window.open(bestMatch.link)
+            })
+        }     
     })
 }
 
