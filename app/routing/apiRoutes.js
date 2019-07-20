cf9f38fa8888845364c999d0a2c45a59f08a9da0
vilagen <vilagen@yahoo.com>
2019-07-20 15:19:56 -0400

    worked on better logic to find match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,40 +4,62 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res){
         res.json(friends)
     })
+    
+    app.get('/api/match/:id', function(req, res) {
+       const id = req.params.id //0
+       const user = friends[id]; // David ...
+
+       const friendScore = [];
+       
+       // make sure user isn't included in score comparison 
+       const otherFriends = friends.filter((friend, id) => id !== user.id);
+
+        // loop over friends
+        for(var i = 0; i < otherFriends.length; i++){
+            const scores = otherFriends[i].scores
+            const userScores = user.scores
+
+            const differeneFromUser = scores.reduce(function(difference, score, index) {
+                // calculate the delta between user score and friend score
+                const currentScoreDifference = (parseInt(Math.abs(userScores[index] - score)));
+                // add to total
+                return difference +  currentScoreDifference;
+            }, 0);
+
+            // push friends with score to new array 
+            friendScore.push({name: otherFriends[i].name, link: otherFriends[i].link, score: differeneFromUser})
+        }
+
+    
+        let bestMatch = friendScore[0];
+
+        // loop over friendScore array 
+        for(let i = 0; i < friendScore.length; i++){
+
+            if(friendScore[i].score < bestMatch.score) {
+                bestMatch = friendScore[i];
+            }
+        }
+
+      res.json(bestMatch);
+    });
 
     app.post("/api/friends", function(req, res) {
         // input logic 
+        const  user = req.body
+        friends.push(user);
 
-            user = req.body
+        res.json({
+            id: friends.length - 1
+        });
+    })
 
-            var friendScore = []
-            var total = []
-            for(var i = 0; i < friends.length; i++){
-                scores = friends[i].scores
-                userScores = user.scores
-                for( var j = 0; j < scores.length; j++) {
-                    total.push(parseInt(Math.abs(userScores[j] - scores[j])))
-                }
-                let sum = total.reduce((acc, val) => {
-                    return acc + val
-                });
-                friendScore.push({name: friends[i].name, link: friends[i].link, score: sum})
-                var total = []
-            }
-            friendScore.pop()
-            friendFinder(friendScore)
-            
-        })
+    
+    var friendScore = []
+    
+    friendFinder(friendScore)
+
         
-        function friendFinder(arr){
-            var bestMatch = arr[0]
-            for(var i = 0; i < arr.length; i++){
-                if(arr[i].score < bestMatch.score) {
-                    var bestMatch = arr[i]
-                    var bestName = bestMatch.name
-                    var bestLink = bestMatch.link
-                }
-            }
             $("#matchedFriend").text("Name: " + bestMatch.name)
             $("#matchedLink").attr("href", bestMatch.link)
             $("#matchedLink").on("click", function(event){
