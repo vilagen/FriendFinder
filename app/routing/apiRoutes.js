56dd6eafcfdc341e1720142009888c2386a89735
vilagen <vilagen@yahoo.com>
2019-07-21 09:54:21 -0400

    Got program to run as intended. For now photos are only displayed as a seperate link.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,75 +5,32 @@ module.exports = function(app) {
         res.json(friends)
     })
     
-    // This page is a mess, so here is what I plan to do:
-
-    // use code I had on front end and implement on backend since it did work
-        // make a variable like friend = req.body, which hopefully will contain user's information
-        // user that, along with friends and looping to use the code already made under this post
-        // use a post to push bestMatch to friends
-        // respond with bestMatch
-
-    // calling only one specific api result for the best
-    app.get('/api/match/:id', function(req, res) {
-       const id = req.params.id //0
-       console.log(id)
-       const user = friends[id]; // David ...
-       console.log("friends: " + user)
-    //    const user = friends[id]; // David ...
-
-       const friendScore = [];
-       
-    //    make sure user isn't included in score comparison 
-    //    const otherFriends = friends.filter((friends, id) => id !== user.id);
-    //    const otherFriends = friends.filter(function(friend, id) {
-    //    id !== user.id
-    // })
-    //    const otherFriends = friends.filter(friend =>friend.id);
-
-        const otherFriends = friends.filter(friend=> friend[id] !== id)
-        console.log("This is for otherfriends " + otherFriends)
-
-        // loop over friends
-        for(let i = 0; i < otherFriends.length; i++){
-            const scores = otherFriends[i].scores
-            const userScores = user.scores
-
-            const differeneFromUser = scores.reduce(function(difference, score, index) {
-                console.log("differenceFromUser: " + differenceFromUser)
-                // calculate the delta between user score and friend score
-                const currentScoreDifference = (parseInt(Math.abs(userScores[index] - score)));
-                console.log("currentScoredifference: " + currentScoreDifference)
-                // add to total
-                return difference +  currentScoreDifference;
-            }, 0);
-
-            // push friends with score to new array 
-            friendScore.push({name: otherFriends[i].name, link: otherFriends[i].link, score: differeneFromUser})
-        }
-
-    
-        let bestMatch = friendScore[0];
-
-        // loop over friendScore array 
-        for(let i = 0; i < friendScore.length; i++){
-
-            if(friendScore[i].score < bestMatch.score) {
-                bestMatch = friendScore[i];
-            }
-        }
-
-      res.json(bestMatch);
-      console.log("This is on server bestMatch " + bestMatch)
-      
-    });
-
     app.post("/api/friends", function(req, res) {
-        const  user = req.body
-        friends.push(user);
+        var user = req.body
+        var total = []
+        var friendScore = []
+    
+        for(let i = 0; i < friends.length; i++){
+            scores = friends[i].scores
+            userScores = user.scores
 
-        // when getting a response, make sure not to include current user
-        res.json({
-            id: friends.length - 1
-        });
+            for( let j = 0; j < scores.length; j++) {
+                total.push(parseInt(Math.abs(userScores[j] - scores[j])))
+            }
+            sum = total.reduce((acc, val) => {return acc + val});
+            friendScore.push({name: friends[i].name, link: friends[i].link, score: sum})
+            var total = []
+            console.log(friendScore)
+        }
+        
+            var bestMatch = friendScore[0]
+            for(let i = 0; i < friendScore.length; i++){
+                if(friendScore[i].score < bestMatch.score) {
+                    var bestMatch = friendScore[i]
+                }
+            }
+
+        res.json(bestMatch)
+        friends.push(user);
     })
 }
---
+++ app/routing/dummyAPIroute.js
@@ -0,0 +1,70 @@
+    // this page is not meant to be used on live page.
+    
+    // originally someone helped me create cleaner code to get the bestMatch results. However,
+    // I wasn't able to understand how to implement it. Saving it here to try to go over this
+    // in this future.
+    
+    var friends = require("../data/friends")
+
+    module.exports = function(app) {
+    app.get("/api/friends", function(req, res){
+        res.json(friends)
+    })
+    
+    // calling only one specific api result for the best
+    app.get('/api/match/:id', function(req, res) {
+       const id = req.params.id //0
+       console.log(id)
+       const user = friends[id]; // David ...
+       console.log("friends: " + user)
+       const user = friends[id]; // David ...
+
+       const friendScore = [];
+       
+    //    make sure user isn't included in score comparison 
+    //    const otherFriends = friends.filter((friends, id) => id !== user.id);
+    //    const otherFriends = friends.filter(function(friend, id) {
+    //    id !== user.id
+    // })
+    //    const otherFriends = friends.filter(friend =>friend.id);
+
+        const otherFriends = friends.filter(friend=> friend[id] !== id)
+        console.log("This is for otherfriends " + otherFriends)
+
+        // loop over friends
+        for(let i = 0; i < otherFriends.length; i++){
+            const scores = otherFriends[i].scores
+            const userScores = user.scores
+
+            const differeneFromUser = scores.reduce(function(difference, score, index) {
+                console.log("differenceFromUser: " + differenceFromUser)
+                // calculate the delta between user score and friend score
+                const currentScoreDifference = (parseInt(Math.abs(userScores[index] - score)));
+                console.log("currentScoredifference: " + currentScoreDifference)
+                // add to total
+                return difference +  currentScoreDifference;
+            }, 0);
+
+            // push friends with score to new array 
+            friendScore.push({name: otherFriends[i].name, link: otherFriends[i].link, score: differeneFromUser})
+        }
+
+    
+        let bestMatch = friendScore[0];
+
+        // loop over friendScore array 
+        for(let i = 0; i < friendScore.length; i++){
+
+            if(friendScore[i].score < bestMatch.score) {
+                bestMatch = friendScore[i];
+            }
+        }
+
+      res.json(bestMatch);
+      console.log(bestMatch)
+
+    });
+
+    
+
+}
